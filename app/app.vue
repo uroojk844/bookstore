5772b2a9547d89da20b503d77d9ef6b11ae5f7da
urooj khan <uroojk844@gmail.com>
2026-08-21 15:44:02 +0530

    adding base app structure

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-red-500">
+    Hi
+  </div>
+
+  <Icon name="uil:times" />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ["~/css/main.css"],
+
+  vite: {
+  plugins: [
+      tailwindcss(),
+    ]
+  },
+
+  runtimeConfig: {
+    public: {
+      googleBooksApi: 'https://www.googleapis.com/books/v1/volumes',
+    },
+    googleBooksApiKey: process.env.GOOGLE_BOOKS_API_KEY || ''
+  },
+  modules: ["@nuxt/fonts", "@nuxt/icon"]
+})
